88543918086e9b52225b3ba92aa999acc73765a6
pietroolivi <pietroolivi@icloud.com>
2023-02-17 15:23:48 +0100

    Implemented the class with 3rd level data

---
+++ src/main/java/game/minigame/whacamoleminigame/LevelThree.java
@@ -0,0 +1,20 @@
+package game.minigame.whacamoleminigame;
+
+/**
+ * Class that contains values related to 
+ * the third level of the whac-a-mole game.
+ */
+public class LevelThree implements Level {
+    private static final int MAX_OBJS_OUT_AT_ONCE = 3;
+    private static final long EXIT_TIME = 3000L;
+    
+    @Override
+    public int getMaxObjsSimultaneouslyOut() {
+        return LevelThree.MAX_OBJS_OUT_AT_ONCE;
+    }
+
+    @Override
+    public long getMoleExitPeriod() {
+        return LevelThree.EXIT_TIME;
+    }
+}
