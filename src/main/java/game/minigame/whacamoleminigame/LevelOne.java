b10a8d55e10145dcd7ea878bfc9deff600e25b28
pietroolivi <pietroolivi@icloud.com>
2023-02-17 15:21:23 +0100

    Implemented the class with 1st level data

---
+++ src/main/java/game/minigame/whacamoleminigame/LevelOne.java
@@ -0,0 +1,20 @@
+package game.minigame.whacamoleminigame;
+
+/**
+ * Class that contains values related to 
+ * the first level of the whac-a-mole game.
+ */
+public class LevelOne implements Level {
+    private static final int MAX_OBJS_OUT_AT_ONCE = 1;
+    private static final long EXIT_TIME = 5000L;
+    
+    @Override
+    public int getMaxObjsSimultaneouslyOut() {
+        return LevelOne.MAX_OBJS_OUT_AT_ONCE;
+    }
+
+    @Override
+    public long getMoleExitPeriod() {
+        return LevelOne.EXIT_TIME;
+    }
+}
