1594ebad14c9c7eb9afa1d52f49ec62a2472e054
pietroolivi <pietroolivi@icloud.com>
2023-02-19 18:03:01 +0100

    Added interface that deals with random mole/bomb draw

---
+++ src/main/java/game/minigame/whacamoleminigame/DrawStrategy.java
@@ -0,0 +1,23 @@
+package game.minigame.whacamoleminigame;
+
+import java.util.Set;
+import game.gameobject.GameObject;
+
+/**
+ * Interface used as a strategy pattern, containing the 
+ * interchangeable algorithm that creates moles/bombs in random 
+ * number & positions (also assigning them an arbitrary appearance 
+ * time) within the limits set by the current level of difficulty.
+ */
+public interface DrawStrategy {
+
+    /**
+     * Method for randomly drawing moles and/or bombs.
+     * 
+     * @param currentLevel from which we obtain the exit time (speed) of the 
+     * GameObjects and the maximum number of them with overlapping exit times
+     * @param numHoles number of holes in the playing field
+     * @return a Set containing the new GameObjects to add to the game
+     */
+    Set<GameObject> draw(Level currentLevel, int numHoles);
+}
