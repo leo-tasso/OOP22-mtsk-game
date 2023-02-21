229c2c89f5ab56b0eee778acc87b725f87ec77ce
pietroolivi <pietroolivi@icloud.com>
2023-02-21 19:36:54 +0100

    Random draw method, v1.0

--- src/main/java/game/minigame/whacamoleminigame/DrawStrategyImpl.java
+++ src/main/java/game/minigame/whacamoleminigame/DrawStrategyImpl.java
@@ -14,4 +14,27 @@ import game.gameobject.whacamoleobjects.Mole;
  */
 public class DrawStrategyImpl implements DrawStrategy {
     
+    @Override
+    public Set<GameObject> draw(Level currLevel, long upperBond, int numHoles) {
+        final Set<GameObject> newGameObjs = new HashSet<>();
+        final int maxObjs = currLevel.getMaxObjsSimultaneouslyOut();
+        final Map<Integer, Boolean> isHoleBusy = new HashMap<>();
+        for (int i = 1; i <= numHoles; i++) {
+            isHoleBusy.put(i, false);
+        }
+        final Random rand = new Random();
+        final int nMoles = rand.nextInt(maxObjs + 1);
+        final int nBombs = rand.nextInt(maxObjs - nMoles + 1); 
+        for (int i = 0; i < nMoles; i++) {           
+            final long lowerBond = System.currentTimeMillis();
+            final long appearanceTime = (int) Math.random() * (upperBond - lowerBond) + lowerBond;
+            newGameObjs.add(new Mole(currLevel.getObjExitPeriod(), appearanceTime, assignHole(isHoleBusy)));
+        }
+        for (int i = 0; i < nBombs; i++) {
+            final long lowerBond = System.currentTimeMillis();
+            final long appearanceTime = (int) Math.random() * (upperBond - lowerBond) + lowerBond;
+            newGameObjs.add(new Bomb(currLevel.getObjExitPeriod(), appearanceTime, assignHole(isHoleBusy)));
+        }
+        return newGameObjs; 
+    }
 }
