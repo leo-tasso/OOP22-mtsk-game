d2761173b065cb6ae844a3efe2aca314243000f9
pietroolivi <pietroolivi@icloud.com>
2023-02-21 19:30:22 +0100

    New class for simple draw algorithm

---
+++ src/main/java/game/minigame/whacamoleminigame/DrawStrategyImpl.java
@@ -0,0 +1,17 @@
+package game.minigame.whacamoleminigame;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import game.gameobject.GameObject;
+import game.gameobject.whacamoleobjects.Bomb;
+import game.gameobject.whacamoleobjects.Mole;
+
+/**
+ * Class that implements a simple draw of GameObjects.
+ */
+public class DrawStrategyImpl implements DrawStrategy {
+    
+}
