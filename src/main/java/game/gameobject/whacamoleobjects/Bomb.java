0f4df92611acbf8ce35734906e7c80317570c0f4
pietroolivi <pietroolivi@icloud.com>
2023-02-23 16:39:25 +0100

    Bomb class archetype

---
+++ src/main/java/game/gameobject/whacamoleobjects/Bomb.java
@@ -0,0 +1,25 @@
+
+package game.gameobject.whacamoleobjects;
+
+import api.Point2D;
+import api.Vector2D;
+import game.gameobject.GameObject;
+
+/**
+ * TODO.
+ */
+public class Bomb extends GameObject {
+
+    /**
+     * The constructor for the bomb.
+     *
+     * @param coor the initial coordinates of the bomb.
+     * @param vel the initial speed of the bomb.
+     */
+    public Bomb(final Point2D coor, final Vector2D vel) {
+        super(coor, vel);
+        //TODO Auto-generated constructor stub
+    }
+
+
+}
