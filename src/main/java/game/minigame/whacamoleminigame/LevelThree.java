237dea0045b99f8badb58f0291035e2d7ec0a9b7
pietroolivi <pietroolivi@icloud.com>
2023-02-25 19:22:24 +0100

    Additional values for lv 3

--- src/main/java/game/minigame/whacamoleminigame/LevelThree.java
+++ src/main/java/game/minigame/whacamoleminigame/LevelThree.java
@@ -1,5 +1,6 @@
 package game.minigame.whacamoleminigame;
 
+import api.TimeInterval;
 import api.Vector2D;
 
 /**
@@ -8,6 +9,8 @@ import api.Vector2D;
  */
 public class LevelThree implements Level {
     private static final int MAX_OBJS_OUT_AT_ONCE = 3;
+    private static final double VECTOR_ORDINATE = 100;
+    private static final long MAX_WAIT_TO_SPAWN = 5000L;
     
     @Override
     public int getMaxObjsSimultaneouslyOut() {
@@ -16,6 +19,11 @@ public class LevelThree implements Level {
 
     @Override
     public Vector2D getObjSpeed() {
-        return new Vector2D(0, 100);
+        return new Vector2D(0, LevelThree.VECTOR_ORDINATE);
+    }
+
+    @Override
+    public TimeInterval getSpawnWaitingTime() {
+        return new TimeInterval(0, LevelThree.MAX_WAIT_TO_SPAWN);
     }
 }
