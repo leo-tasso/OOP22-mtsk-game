e96735dc3f527e47973ccdfb88f919f645557f02
pietroolivi <pietroolivi@icloud.com>
2023-03-09 10:39:00 +0100

    New class that applies user input to GameObjs individually

---
+++ src/main/java/game/controlling/WamInputModel.java
@@ -0,0 +1,17 @@
+package game.controlling;
+
+import api.Vector2D;
+import game.gameobject.GameObject;
+import game.gameobject.whacamoleobjects.Status;
+import game.gameobject.whacamoleobjects.WamObject;
+import game.minigame.whacamoleminigame.WhacAMole;
+
+/**
+ * Class responsible for updating the object (that contains an 
+ * instance of it), according to the input received from the View, 
+ * notifying in particular whether it has been squashed or not.
+ */
+public class WamInputModel implements InputModel {
+
+
+}
