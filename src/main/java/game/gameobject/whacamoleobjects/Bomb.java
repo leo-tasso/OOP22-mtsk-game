13f93b5b42f87bec5d135f8c4d516e720f069a57
pietroolivi <pietroolivi@icloud.com>
2023-03-12 17:58:43 +0100

    Constructor for Bomb.java

--- src/main/java/game/gameobject/whacamoleobjects/Bomb.java
+++ src/main/java/game/gameobject/whacamoleobjects/Bomb.java
@@ -1,24 +1,31 @@
 package game.gameobject.whacamoleobjects;
 
+import game.controlling.InputModel;
+import game.gameobject.AspectModel;
+import game.gameobject.PhysicsModel;
 import game.minigame.whacamoleminigame.Level;
 
+/**
+ * Class that models the bomb character in the Whac-a-Mole minigame.
+ */
 public class Bomb extends WamObject {
 
-    public Bomb(long appearanceTime, Level currentLevel, int holeNumber) {
-        super(appearanceTime, currentLevel, holeNumber);
-        //TODO Auto-generated constructor stub
+    /**
+     * Simple constructor aimed at initializing Bomb fields.
+     * 
+     * @param appearanceTime the time at which it is scheduled to appear
+     * @param currentLevel   the level to which the object belongs
+     * @param holeNumber     the identifier of the hole assigned to the bomb
+     * @param physicsModel   the physics model of the WamObject
+     * @param aspectModel    the aspect model of the bomb 
+     * @param inputModel     the input model of the WamObject
+     */
+    public Bomb(long appearanceTime, 
+                Level currentLevel, 
+                int holeNumber,
+                PhysicsModel physicsModel, 
+                AspectModel aspectModel, 
+                InputModel inputModel) {
+        super(appearanceTime, currentLevel, holeNumber, physicsModel, aspectModel, inputModel);
     }
-
-    @Override
-    public boolean isGameOver() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isGameOver'");
-    }
-
-    @Override
-    public boolean isStillInUse() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isStillInUse'");
-    }
-    
 }
