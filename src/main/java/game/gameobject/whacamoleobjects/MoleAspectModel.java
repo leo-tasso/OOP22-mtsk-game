4de4f8e06012ee5a385055cf477e24876072e1ec
pietroolivi <pietroolivi@icloud.com>
2023-03-13 11:38:37 +0100

    Implemented update() method in  MoleAspectModel

---
+++ src/main/java/game/gameobject/whacamoleobjects/MoleAspectModel.java
@@ -0,0 +1,29 @@
+package game.gameobject.whacamoleobjects;
+
+import game.gameobject.AspectModel;
+import game.gameobject.GameObject;
+import game.view.Drawings;
+
+/**
+ * The AspectModel for the Mole object.
+ */
+public class MoleAspectModel implements AspectModel {
+
+    /**
+     * I order to draw the Mole only if it has actually 
+     * entered the game, i.e. if it has already been 
+     * spawned, in which case I distinguish the object's 
+     * appearance based on whether it has been hit or not.
+     */
+    @Override
+    public void update(GameObject object, Drawings drawing) {
+        final Status status = ((WamObject) object).getStatus();
+        if (!status.equals(Status.WAITING)) {
+            if (status.equals(Status.HIT)) {
+                drawing.drawMole(object, true);
+            } else {
+                drawing.drawMole(object, false);
+            }
+        } 
+    }   
+}
