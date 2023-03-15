ac875bd5bba703363b53c4f3916410fec7c5463b
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2023-03-15 20:55:50 +0100

    Added hitbox to Cursor and improved Cursor InputModel

--- src/main/java/game/engine/gameobject/flappybirdalikeobjects/Cursor.java
+++ src/main/java/game/engine/gameobject/flappybirdalikeobjects/Cursor.java
@@ -2,16 +2,17 @@ package game.engine.gameobject.flappybirdalikeobjects;
 
 import api.Point2D;
 import api.Vector2D;
-import game.controlling.DirectionalInput;
 import game.controlling.Input;
 import game.controlling.InputModel;
 import game.engine.gameobject.GameObject;
+import game.engine.gameobject.hitboxmodel.CircleHitBoxModel;
 
 /**
  * A cursor you can move up and down.
  *
  */
 public class Cursor extends GameObject {
+    private static final double UPWARD_SPEED = -8;
 
     /**
      * The constructor for the cursor.
@@ -23,19 +24,26 @@ public class Cursor extends GameObject {
      */
     public Cursor(final Point2D coor, final Vector2D vel, final double size, final double xSpeed) {
         super(coor, vel);
+        final double radius = size / Math.sqrt(3) * 3 / 4;
         this.setInputModel(new InputModel() {
 
-            private final InputModel standard = new DirectionalInput();
+            private boolean hold;
 
             @Override
             public void update(final GameObject obj, final Input c, final long elapsedTime) {
-                if (c.isMoveDown() || c.isMoveUp()) {
-                    standard.update(obj, c, elapsedTime);
+                if (c.isJump() && !hold) {
+                    this.hold = true;
+                    obj.setVel(new Vector2D(0, UPWARD_SPEED * elapsedTime));
+                }
+
+                if (!c.isJump() && hold) {
+                    this.hold = false;
                 }
             }
 
         });
         this.setPhysicsModel(new FlappyPhysics(size));
         this.setAspectModel(new CursorAspect(size, xSpeed));
+        this.setHitBoxModel(new CircleHitBoxModel(radius));
     }
 }
