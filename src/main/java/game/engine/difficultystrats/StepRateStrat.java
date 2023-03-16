7270630ba11392ab8f1bbea543dc336c544dbc5d
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2023-03-16 17:59:55 +0100

    Implemented StepRateStrat

--- src/main/java/game/engine/difficultystrats/StepRateStrat.java
+++ src/main/java/game/engine/difficultystrats/StepRateStrat.java
@@ -1,25 +1,39 @@
 package game.engine.difficultystrats;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 /**
  * Spawn strategy that follows a step-like function (similar to a floor function)
- * until it reaches the maximum amount of steps.
+ * until it reaches the maximum value allowed.
  */
 public class StepRateStrat implements Function<Long, Integer> {
 
     private int numSteps;
+    private int valueStep;
     private long denominator;
 
-    public StepRateStrat(final int numSteps, final long denominator) {
+    /**
+     * Constructor for StepRateStrat.
+     * 
+     * @param numSteps the number of steps required to reach the maximum value
+     * @param valueStep the difference between two adjacent steps
+     * @param denominator the distance to get to the next step
+     */
+    public StepRateStrat(final int numSteps, final int valueStep, final long denominator) {
         this.numSteps = numSteps;
+        this.valueStep = valueStep;
         this.denominator = denominator;
     }
 
+    /**
+     * Method to get the x coordinate the final obstacle has to reach
+     * before spawning a new one.
+     */
     @Override
-    public Integer apply(Long arg0) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'apply'");
+    public Integer apply(Long totalElapsed) {
+        return Optional.of(totalElapsed / denominator)
+                .map(x -> (int) (x >= numSteps ? numSteps * valueStep : x * valueStep))
+                .get();
     }
-    
 }
