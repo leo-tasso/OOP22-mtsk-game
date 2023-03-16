66e8d925c67ac8bf23faaaf2a05802cdacc8d2e1
Leonardo Tassinari <leonardo.tassinari6@studio.unibo.it>
2023-03-16 10:42:35 +0100

    Renamed catchthesquare test folder

---
+++ src/test/java/minigamestests/catchthesquaretest/BombTest.java
@@ -0,0 +1,29 @@
+package minigamestests.catchthesquaretest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import api.ColorRGB;
+import api.Point2D;
+import game.engine.gameobject.catchthesqareobjects.Bomb;
+
+/**
+ * Test to check the {@link Bomb} {@link GameObject} properties.
+ */
+class BombTest {
+    private static final int SIDE = 20;
+    private static final int ITERATIONS = 100;
+    private static final long DELTA = 15;
+
+    @Test
+    void timeDec() {
+        final Bomb b = new Bomb(Point2D.origin(), SIDE, ColorRGB.black());
+        final long start = b.getTimer();
+        for (int i = 0; i < ITERATIONS; i++) {
+            b.updatePhysics(DELTA, null);
+        }
+        assertTrue(start > b.getTimer());
+    }
+
+}
