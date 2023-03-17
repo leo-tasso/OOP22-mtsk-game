75ba64e756b5a87abc0a3f680f0374a4a7f79bd7
pietroolivi <pietroolivi@icloud.com>
2023-03-17 16:53:50 +0100

    Class that deals with the creation and placement of holes

---
+++ src/main/java/game/engine/minigame/whacamoleminigame/HolesGenerator.java
@@ -0,0 +1,25 @@
+package game.engine.minigame.whacamoleminigame;
+
+import java.util.List;
+import game.engine.gameobject.whacamoleobjects.WamObject;
+
+/**
+ * Class that manages the creation and location of dens from 
+ * which moles and bombs come and go in the game Whac-a-Mole.
+ */
+public class HolesGenerator {
+
+    /**
+     * Taking the dimensions (in coor) of the playing field and 
+     * the number of holes in the game, places the holes in a 
+     * "table" where the number of rows of columns is the same.
+     * 
+     * @param numHoles the number of dens to creates
+     * @return
+     */
+    public List<WamObject> generate(int numHoles) {
+        return null;
+        // ADD PLAYING FIELD DIM TO CONSTRUCTOR
+        // NULL_INPUT_CLASS TO HOLES
+    }
+}
