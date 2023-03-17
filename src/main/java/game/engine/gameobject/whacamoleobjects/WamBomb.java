a855daf30d777ae59746e81df4b008de01e351f8
pietroolivi <pietroolivi@icloud.com>
2023-03-17 16:09:02 +0100

    Added startCoor in Bomb's constructor

--- src/main/java/game/engine/gameobject/whacamoleobjects/WamBomb.java
+++ src/main/java/game/engine/gameobject/whacamoleobjects/WamBomb.java
@@ -1,5 +1,6 @@
 package game.engine.gameobject.whacamoleobjects;
 
+import api.Point2D;
 import game.controlling.InputModel;
 import game.engine.gameobject.AspectModel;
 import game.engine.gameobject.PhysicsModel;
@@ -13,6 +14,7 @@ public class WamBomb extends WamObject {
     /**
      * Simple constructor aimed at initializing Bomb fields.
      * 
+     * @param startCoor      the initial coordinates
      * @param appearanceTime the time at which it is scheduled to appear
      * @param currentLevel   the level to which the object belongs
      * @param holeNumber     the identifier of the hole assigned to the bomb
@@ -20,13 +22,14 @@ public class WamBomb extends WamObject {
      * @param aspectModel    the aspect model of the bomb 
      * @param inputModel     the input model of the WamObject
      */
-    public WamBomb(final long appearanceTime, 
+    public WamBomb(final Point2D startCoor,
+                final long appearanceTime, 
                 final Level currentLevel, 
                 final int holeNumber,
                 final PhysicsModel physicsModel, 
                 final AspectModel aspectModel, 
                 final InputModel inputModel) {
-        super(appearanceTime, currentLevel, holeNumber, physicsModel, aspectModel, inputModel);
+        super(startCoor, appearanceTime, currentLevel, holeNumber, physicsModel, aspectModel, inputModel);
     }
 
     /**
