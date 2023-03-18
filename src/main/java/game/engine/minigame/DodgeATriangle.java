20c408ac2b61524b940fd2a9b1a6cafd8dc5172d
pietroolivi <pietroolivi@icloud.com>
2023-03-18 10:16:50 +0100

    Added slots obj to D-a-T class

--- src/main/java/game/engine/minigame/DodgeATriangle.java
+++ src/main/java/game/engine/minigame/DodgeATriangle.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.stream.Stream;
+
 import api.Point2D;
 import api.Vector2D;
 import game.controlling.DodgerInputModel;
+import game.controlling.NullInput;
 import game.engine.gameobject.GameObject;
+import game.engine.gameobject.SimplePhysics;
 import game.engine.gameobject.dodgeatriangleobjects.DatTriangle;
 import game.engine.gameobject.dodgeatriangleobjects.Dodger;
+import game.engine.gameobject.dodgeatriangleobjects.SlotAspect;
 import game.engine.gameobject.hitboxmodel.Collider;
 import game.engine.gameobject.hitboxmodel.ColliderImpl;
 
@@ -20,6 +25,7 @@ import game.engine.gameobject.hitboxmodel.ColliderImpl;
  */
 public class DodgeATriangle implements Minigame {
 
+    private static final int INITIAL_X = 800;
     private static final int INITIAL_Y = 450;
     private static final int SIDE_LENGTH = 140;
     private static final int SPAWN_LEFT = - SIDE_LENGTH;
@@ -28,6 +34,7 @@ public class DodgeATriangle implements Minigame {
     private static final int NUM_SLOTS = 5;
 
     private final List<GameObject> l = new ArrayList<>();
+    private final GameObject slots;
     private final Collider c = new ColliderImpl();
     private final Random rand = new Random();
     private long totalElapsed;
@@ -39,6 +46,11 @@ public class DodgeATriangle implements Minigame {
     public DodgeATriangle() {
         this.l.add(new Dodger(INITIAL_Y, SIDE_LENGTH,
                 new DodgerInputModel(SIDE_LENGTH, INITIAL_Y)));
+        this.slots = new GameObject(new Point2D(INITIAL_X, INITIAL_Y), ENEMY_SPEED);
+        this.slots.setAspectModel(new SlotAspect(SIDE_LENGTH, new Point2D(INITIAL_X, INITIAL_Y), NUM_SLOTS));
+        this.slots.setInputModel(new NullInput());
+        this.slots.setPhysicsModel(new SimplePhysics());
+        this.slots.setVel(Vector2D.nullVector());
     }
 
     /**
@@ -60,7 +72,7 @@ public class DodgeATriangle implements Minigame {
         this.totalElapsed += elapsed;
         if (l.size() == 1) {
             int enemyY = Optional.of(rand.nextInt(NUM_SLOTS))
-                    .map(y -> INITIAL_Y + (y - 3) * SIDE_LENGTH)
+                    .map(y -> INITIAL_Y + (y - 2) * SIDE_LENGTH)
                     .get();
             int enemyX = Optional.of(rand.nextInt(2))
                     .map(x -> x == 0 ? SPAWN_LEFT : SPAWN_RIGHT)
@@ -79,7 +91,8 @@ public class DodgeATriangle implements Minigame {
      */
     @Override
     public List<GameObject> getObjects() {
-        return new ArrayList<>(this.l);
+        return Stream.concat(l.stream(), Stream.of(slots)).toList();
+        // return new ArrayList<>(this.l);
     }
 
     /**
