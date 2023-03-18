70cfc5553211d06b87dcdd90ee91b9f96fc8c928
pietroolivi <pietroolivi@icloud.com>
2023-03-18 22:55:38 +0100

    Implemented hitboxCheck() test
    Co-authored-by: Lorenzo Dalmonte <Dalmont-code@users.noreply.github.com>

--- src/test/java/minigamestests/dodgeatriangletest/DodgeATriangleTest.java
+++ src/test/java/minigamestests/dodgeatriangletest/DodgeATriangleTest.java
@@ -1,14 +1,16 @@
 package minigamestests.dodgeatriangletest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.function.Consumer;
+
 import org.junit.jupiter.api.Test;
 
 import api.Point2D;
 import game.controlling.Input;
 import game.controlling.KeyboardInput;
-import game.engine.gameobject.dodgeatriangleobjects.SlotAspect;
 import game.engine.minigame.DodgeATriangle;
 import game.engine.minigame.Minigame;
-import javafx.scene.effect.Light.Point;
 
 /**
  * Unit test for the minigame DodgeATriangle.
@@ -16,17 +18,31 @@ import javafx.scene.effect.Light.Point;
 class DodgeATriangleTest {
 
     private static final long ELAPSED_TIME = 10L;
+    private static final int CYCLES = 400;
+    private static final int LIMIT_LOW = 170;
+    private static final int LIMIT_HIGH = 730;
+    private static final int MOVES = 7;
 
+    /**
+     * Elapsed Time must be small enough to be able to capture the 
+     * moment in which the collision occurs (if it were too large, 
+     * the triangle would risk "teleporting" from before to after 
+     * the Dodger) while CYCLES must be large enough to be able to 
+     * arrive at the moment in which the triangle hits the Dodger.
+     */
     @Test
     void hitboxCheck() {
         final Minigame m = new DodgeATriangle();
-        final Point2D center; /* 
-        while(m.getObjects().size() == 1 
-            && m.getObjects().get(0).getCoor().getX() != ) {
+        final Point2D center = m.getObjects().get(0).getCoor();
+
+        while (m.getObjects().size() == 2 // The last elem is an istance of Slots
+            || center.getY() != m.getObjects().get(1).getCoor().getY()) {
             m.compute(ELAPSED_TIME);
         }
-        */
+        for (int i = 0; i < CYCLES; i++) {
+            m.compute(ELAPSED_TIME);
+            m.isGameOver();
+        }
+        assertTrue(m.isGameOver());
     }
-
-
 }
