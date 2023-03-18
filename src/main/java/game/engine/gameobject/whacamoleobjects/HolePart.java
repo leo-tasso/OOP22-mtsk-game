558f6555988d1b3ace3d2a6aebdbd86432c0034c
pietroolivi <pietroolivi@icloud.com>
2023-03-18 16:26:11 +0100

    Changed class name to be consistent with classes that extend it

--- src/main/java/game/engine/gameobject/whacamoleobjects/HolePart.java
+++ src/main/java/game/engine/gameobject/whacamoleobjects/HolePart.java
@@ -7,22 +7,22 @@ import game.engine.gameobject.PhysicsModel;
 import game.engine.minigame.whacamoleminigame.Level;
 
 /**
- * Class that models the hole item in the Whac-a-Mole minigame.
+ * Class that models an hole's section in the Whac-a-Mole minigame.
  */
-public class Hole extends WamObject {
+public class HolePart extends WamObject {
 
     /**
-     * Simple constructor aimed at initializing Hole fields.
+     * Simple constructor aimed at initializing hole's section fields.
      * 
      * @param coor           the coordinates it will keep for the whole game
      * @param appearanceTime the start time of the game
      * @param currentLevel   unused value
-     * @param holeNumber     the number that identifies it
+     * @param holeNumber     the number that identifies the hole it belongs to
      * @param physicsModel   the physics model of the hole (it doesn't move)
-     * @param aspectModel    the aspect model of the hole 
+     * @param aspectModel    the aspect model of this hole's part 
      * @param inputModel     the null input model of the hole 
      */
-    public Hole(final Point2D coor,
+    public HolePart(final Point2D coor,
                 final long appearanceTime, 
                 final Level currentLevel, 
                 final int holeNumber, 
