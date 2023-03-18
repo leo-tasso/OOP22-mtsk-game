f913f17e43162b3efd8ce025b3842b67f3422131
pietroolivi <pietroolivi@icloud.com>
2023-03-18 16:27:39 +0100

    Created class for HoleUpperPart's Aspect

---
+++ src/main/java/game/engine/gameobject/whacamoleobjects/HoleUpperPartAspectModel.java
@@ -0,0 +1,21 @@
+package game.engine.gameobject.whacamoleobjects;
+
+import game.engine.gameobject.AspectModel;
+import game.engine.gameobject.GameObject;
+import game.view.Drawings;
+
+/**
+ * 
+ */
+public class HoleUpperPartAspectModel implements AspectModel {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void update(GameObject object, Drawings drawing) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+    
+}
