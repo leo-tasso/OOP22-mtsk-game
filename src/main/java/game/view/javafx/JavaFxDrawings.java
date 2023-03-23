96eef8784392f5829a87d552101208f6cb41fa30
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2023-03-23 19:43:26 +0100

    Changed JavaFxDrawings to fill triangle shapes

--- src/main/java/game/view/javafx/JavaFxDrawings.java
+++ src/main/java/game/view/javafx/JavaFxDrawings.java
@@ -126,8 +126,8 @@ public class JavaFxDrawings implements Drawings {
         ys.add(object.getCoor().getY() + radius * Math.sin(rotAngle2));
         ys.add(object.getCoor().getY() + radius * Math.sin(rotAngle));
 
-        gc.setStroke(jfxColor(color));
-        gc.strokePolygon(xs.stream().mapToDouble(x -> x * dimention / coefficient + startingPoint.getX()).toArray(),
+        gc.setFill(jfxColor(color));
+        gc.fillPolygon(xs.stream().mapToDouble(x -> x * dimention / coefficient + startingPoint.getX()).toArray(),
                 ys.stream().mapToDouble(y -> y * dimention / coefficient + startingPoint.getY()).toArray(), 3);
     }
 
