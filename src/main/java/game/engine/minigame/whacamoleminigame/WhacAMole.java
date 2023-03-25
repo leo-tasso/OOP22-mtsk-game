68317425534c5317e49a7912ace3ce62bb129e50
pietroolivi <pietroolivi@icloud.com>
2023-03-25 15:14:19 +0100

    Implemented a method that allows you to edit the list of objects (for testing)

--- src/main/java/game/engine/minigame/whacamoleminigame/WhacAMole.java
+++ src/main/java/game/engine/minigame/whacamoleminigame/WhacAMole.java
@@ -18,7 +18,7 @@ public class WhacAMole implements Minigame {
     public static final int NUM_HOLES = 9;
     private static final int DRAWS_TO_NEXT_LEVEL = 3;
 
-    private final List<WamObject> objs;
+    private List<WamObject> objs;
     private final List<Level> levels; 
     private long currentTime;
     private final DrawStrategy draw;
@@ -167,5 +167,14 @@ public class WhacAMole implements Minigame {
      */
     public long getCurrentTime() {
         return this.currentTime;
+    }
+    
+    /**
+     * 
+     * 
+     * @param objs
+     */
+    public void setObjects(List<WamObject> objs) {
+        this.objs = new ArrayList<>(objs);
     }
 }
