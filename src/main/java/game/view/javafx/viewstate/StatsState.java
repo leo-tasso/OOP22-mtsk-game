02c48bd927dc78885ee36a8b4e7b3b460bbd2af2
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2023-03-30 21:50:41 +0200

    Added code to show statistics graph in StatsState

--- src/main/java/game/view/javafx/viewstate/StatsState.java
+++ src/main/java/game/view/javafx/viewstate/StatsState.java
@@ -1,10 +1,16 @@
 package game.view.javafx.viewstate;
 
+import java.sql.Timestamp;
+import java.util.Map;
 import java.util.Optional;
 
+import api.RecordLoaderImpl;
 import game.view.javafx.JavaFxViewCoordinator;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -17,12 +23,32 @@ public class StatsState implements ViewState {
         view.setGameState(Optional.empty());
         final Button exit = new ButtonFactory(BUTTON_SPACING)
                 .create(stage, "Back to Main Menu", e -> Platform.runLater(() -> new ViewStateMenu(view, stage).display(view, stage)));
-        root = new VBox(BUTTON_SPACING, exit);
+        final Map<Timestamp, Long> scores = new RecordLoaderImpl().getRecords();
+        final LineChart<Number, Number> plot = new LineChart<>(
+                new NumberAxis(0, scores.entrySet().size() + 1, 1),
+                new NumberAxis(0, 50_000, 1_000));
+        plot.setTitle(scores.size() == 0 ? "No Records Found..." : "Records");
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        final MiniCounter counter = new MiniCounter();
+        scores.entrySet()
+                .stream()
+                .sorted((a,b) -> a.getKey().getTime() > b.getKey().getTime() ? 1 : -1)
+                .forEach(x -> series.getData().add(new XYChart.Data<>(counter.getCount(), x.getValue())));
+        plot.getData().add(series);
+        root = new VBox(BUTTON_SPACING, plot, exit);
         root.setAlignment(Pos.CENTER);
     }
 
     @Override
     public void display(JavaFxViewCoordinator jView, Stage stage) {
         stage.getScene().setRoot(root);
+    }
+
+    private class MiniCounter {
+        private int count = 1;
+
+        public int getCount() {
+            return count++;
+        }
     }
 }
