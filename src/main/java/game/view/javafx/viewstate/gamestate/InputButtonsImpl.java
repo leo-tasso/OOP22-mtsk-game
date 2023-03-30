9b32b79cb3ade1265cf2d95056b677c22e28bb49
pietroolivi <pietroolivi@icloud.com>
2023-03-30 10:47:17 +0200

    Made NumPad support more intuitive

--- src/main/java/game/view/javafx/viewstate/gamestate/InputButtonsImpl.java
+++ src/main/java/game/view/javafx/viewstate/gamestate/InputButtonsImpl.java
@@ -14,9 +14,11 @@ public class InputButtonsImpl implements InputButtons {
     private static final int NINE_BUTTON = 57;
     private static final int KEYCODE_NUM_DIFF = 48;
     private static final int NUMPAD_ONE = 97;
+    private static final int NUMPAD_FOUR = 100;
+    private static final int NUMPAD_SIX = 102;
     private static final int NUMPAD_NINE = 105;
     private static final int KEYCODE_NUMPAD_DIFF = 96;
-
+    private static final int DIFF = 6;
 
     /**
      * {@inheritDoc}
@@ -34,8 +36,15 @@ public class InputButtonsImpl implements InputButtons {
                 input.setMoveRight(true);
             } else if (e.getCode().getCode() >= ONE_BUTTON && e.getCode().getCode() <= NINE_BUTTON) {
                 input.setNumberPressed(Optional.of(e.getCode().getCode() - KEYCODE_NUM_DIFF));
-            } else if (e.getCode().getCode() >= NUMPAD_ONE && e.getCode().getCode() <= NUMPAD_NINE) {
+            } else if (e.getCode().getCode() >= NUMPAD_FOUR && e.getCode().getCode() <= NUMPAD_SIX) {
                 input.setNumberPressed(Optional.of(e.getCode().getCode() - KEYCODE_NUMPAD_DIFF));
+            } else if (e.getCode().getCode() >= NUMPAD_ONE && e.getCode().getCode() < NUMPAD_FOUR) {
+                /* Since the placement of the numbers on the numpad is        */
+                /* counterintuitive for the purposes of the Whac-a-Mole game, */
+                /* I consider the layout rather than the numbers on the keys  */
+                input.setNumberPressed(Optional.of(e.getCode().getCode() + DIFF - KEYCODE_NUMPAD_DIFF));
+            } else if (e.getCode().getCode() > NUMPAD_SIX && e.getCode().getCode() <= NUMPAD_NINE) {
+                input.setNumberPressed(Optional.of(e.getCode().getCode() - DIFF - KEYCODE_NUMPAD_DIFF));
             } else if (e.getCode().equals(KeyCode.SPACE)) {
                 input.setJump(true);
             } else if (e.getCode().equals(KeyCode.UP)) {
